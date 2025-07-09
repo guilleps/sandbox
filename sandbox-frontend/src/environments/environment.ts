4665de75ec17b6d3e56068e9327933f2cd017d69
Guillermo Palacios <nothicc04@gmail.com>
2025-07-09 00:38:00 -0500

    ci: :rocket: Configuring frontend for hosting-deploy

--- sandbox-frontend/src/environments/environment.ts
+++ sandbox-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	apiUrl: 'http://127.0.0.1:5001/sandbox-ffdff/us-central1/graphql',
+	apiUrl: 'https://us-central1-sandbox-ffdff.cloudfunctions.net/graphql',
 };
