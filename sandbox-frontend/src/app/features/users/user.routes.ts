d1784d1d0916ce2ddf72cf97f999f07e93789402
Guillermo Palacios <nothicc04@gmail.com>
2025-07-10 17:55:04 -0500

    ci: fix path index.html

--- sandbox-frontend/src/app/features/users/user.routes.ts
+++ sandbox-frontend/src/app/features/users/user.routes.ts
@@ -1,9 +1,13 @@
-import { Routes } from "@angular/router";
-import { UserFormComponent } from "./components/user-form/user-form.component";
+import { Routes } from '@angular/router';
+import { UserFormComponent } from '@app/features/users/components/user-form/user-form.component';
 
 export const userRoutes: Routes = [
-    {
-        path: '',
-        component: UserFormComponent
-    }
+	{
+		path: '',
+		component: UserFormComponent,
+	},
+	{
+		path: '**',
+		redirectTo: '',
+	},
 ];
