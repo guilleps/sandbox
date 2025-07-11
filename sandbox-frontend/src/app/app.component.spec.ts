b69b8f0c069410fb298e536635ff12a96602f57d
Guillermo Palacios <nothicc04@gmail.com>
2025-07-11 13:06:52 -0500

    chore: add semantic-release and commitizen configuration

--- sandbox-frontend/src/app/app.component.spec.ts
+++ sandbox-frontend/src/app/app.component.spec.ts
@@ -27,6 +27,6 @@ describe('AppComponent', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.nativeElement as HTMLElement;
-		expect(compiled.querySelector('h1')?.textContent).toContain('Sandbox');
+		expect(compiled.querySelector('h1')?.textContent).toContain('SandboxV1');
 	});
 });
